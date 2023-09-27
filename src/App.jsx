07330f390e6f82f1fcd6c547c710b6872d1d7981
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-09-27 10:40:32 +0300

    added ag-grid rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,83 @@
 import React from 'react'
 import './App.css'
-import Todotable from './components/Todotable';
-import Todoform from './components/Todoform';
+
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 
 function App() {
-  const [newTodo, setNewTodo] = React.useState({description: "", date: ""});
+  const [newTodo, setNewTodo] = React.useState({description: "", date: "", priority: ""});
   const [todos, setTodos] = React.useState([]);
+  const [columnDefs] = React.useState([
+    { field: "description", sortable: true, filter: true, floatingFilter: true},
+    { field: "priority", sortable: true, filter: true,
+      cellStyle: params => params.value === "High" ? {color: "red"} : {color: "black"},
+      floatingFilter: true
+    },
+    { field: "date", sortable: true, filter: true, floatingFilter: true}
+  ]);
+  const gridRef = React.useRef();
 
   const addTodo = () => {
     setTodos([...todos, newTodo]);
-    setNewTodo({description: "", date: ""});
+    setNewTodo({
+      description: "", 
+      date: "", 
+      priority: ""
+    });
   }
 
-  const deleteTodo = (index) => {
-    setTodos(todos.filter((item, i) => i !== index));
+  const deleteTodo = () => {
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      const row = gridRef.current.getSelectedNodes()[0].id;
+      setTodos(todos.filter((item, index) => row != index));
+    } else {
+      alert("Select at least one row!");
+    }
   }
 
   return (
     <>
       <h3>My Todos</h3>
 
-      <Todoform newTodo={newTodo} setNewTodo={setNewTodo} todos={todos} setTodos={setTodos} addTodo={addTodo} />
+      <input 
+        type="text"
+        id="description"
+        value={newTodo.description} 
+        onChange={event => setNewTodo({...newTodo, description: event.target.value})}
+        placeholder="Description"
+      />
 
-      <Todotable todos={todos} delete={deleteTodo}/>
+      <input 
+        type="text"
+        id="priority"
+        value={newTodo.priority} 
+        onChange={event => setNewTodo({...newTodo, priority: event.target.value})}
+        placeholder="Priority"
+      />
+
+      <input 
+        type="date"
+        id="date"
+        value={newTodo.date} 
+        onChange={event => setNewTodo({...newTodo, date: event.target.value})}
+        placeholder="Date"
+      />
+
+      <button onClick={addTodo}>Add todo</button>
+      <button onClick={deleteTodo}>Delete</button>
+
+      <div className='ag-theme-material' style={{height: 390, width:600}}>
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={params => gridRef.current = params.api}
+          rowSelection='single'
+          rowData={todos}
+          columnDefs={columnDefs}
+          animateRows={true}>
+        </AgGridReact>
+      </div>
     </>
   )
 }
