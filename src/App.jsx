924a56619e11be1ce0e8bd82e7555be4735042d1
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-10-06 17:09:33 +0300

    added datepicker functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-import './App.css'
-
 import { AgGridReact } from 'ag-grid-react';
+import Header from './components/Header';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 
 function App() {
   const [newTodo, setNewTodo] = React.useState({description: "", date: "", priority: ""});
   const [todos, setTodos] = React.useState([]);
   const [columnDefs] = React.useState([
-    { field: "description", sortable: true, filter: true, floatingFilter: true},
+    { field: "description", sortable: true, filter: true, floatingFilter: true, valueFormatter: params => params.data.description},
     { field: "priority", sortable: true, filter: true,
       cellStyle: params => params.value === "High" ? {color: "red"} : {color: "black"},
-      floatingFilter: true
+      floatingFilter: true, valueFormatter: params => params.data.priority
     },
-    { field: "date", sortable: true, filter: true, floatingFilter: true}
+    { field: "date", sortable: true, filter: true, floatingFilter: true, valueFormatter: params => dayjs(params.data.date).format('DD-MM-YYYY')}
   ]);
   const gridRef = React.useRef();
 
@@ -31,54 +39,65 @@ function App() {
   const deleteTodo = () => {
     if (gridRef.current.getSelectedNodes().length > 0) {
       const row = gridRef.current.getSelectedNodes()[0].id;
-      setTodos(todos.filter((item, index) => row != index));
+      setTodos(todos.filter((item, index) => row !== index));
     } else {
       alert("Select at least one row!");
     }
   }
 
+  const changeDate = (date) => {
+    const parsedDate = dayjs(date).toDate();
+    setNewTodo({...newTodo, date: parsedDate})
+  }
+
   return (
-    <>
-      <h3>My Todos</h3>
+    <Container>
+      <Header />
 
-      <input 
-        type="text"
-        id="description"
-        value={newTodo.description} 
-        onChange={event => setNewTodo({...newTodo, description: event.target.value})}
-        placeholder="Description"
-      />
+      <Stack 
+        direction="row" 
+        spacing={2} 
+        alignItems="center" 
+        justifyContent="center"
+        mt={2}
+      >
+        <TextField 
+          type="text"
+          id="description"
+          value={newTodo.description} 
+          onChange={event => setNewTodo({...newTodo, description: event.target.value})}
+          label="Description"
+        />
 
-      <input 
-        type="text"
-        id="priority"
-        value={newTodo.priority} 
-        onChange={event => setNewTodo({...newTodo, priority: event.target.value})}
-        placeholder="Priority"
-      />
+        <TextField 
+          type="text"
+          id="priority"
+          value={newTodo.priority} 
+          onChange={event => setNewTodo({...newTodo, priority: event.target.value})}
+          label="Priority"
+        />
 
-      <input 
-        type="date"
-        id="date"
-        value={newTodo.date} 
-        onChange={event => setNewTodo({...newTodo, date: event.target.value})}
-        placeholder="Date"
-      />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker value={newTodo.date} onChange={date => changeDate(date)} />
+        </LocalizationProvider>
 
-      <button onClick={addTodo}>Add todo</button>
-      <button onClick={deleteTodo}>Delete</button>
+        <Button variant='contained' onClick={addTodo}>Add todo</Button>
+        <Button variant='contained' color='error' onClick={deleteTodo}>Delete</Button>
+      </Stack>
 
-      <div className='ag-theme-material' style={{height: 390, width:600}}>
-        <AgGridReact
-          ref={gridRef}
-          onGridReady={params => gridRef.current = params.api}
-          rowSelection='single'
-          rowData={todos}
-          columnDefs={columnDefs}
-          animateRows={true}>
-        </AgGridReact>
-      </div>
-    </>
+      <Stack alignItems="center" justifyContent="center">
+        <div className='ag-theme-material' style={{height: 390, width:600}}>
+          <AgGridReact
+            ref={gridRef}
+            onGridReady={params => gridRef.current = params.api}
+            rowSelection='single'
+            rowData={todos}
+            columnDefs={columnDefs}
+            animateRows={true}>
+          </AgGridReact>
+        </div>
+      </Stack>
+    </Container>
   )
 }
 
